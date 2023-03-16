0c82092a2723de4fadcd65a029bc85b18e36cb02
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2023-03-16 23:16:53 +0200

    file

---
+++ src/components/ContactList/ContactListItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ContactItem, Button, Text } from './ContactList.styled'
+
+const ContactListItem = ({ contactsItem, onDeleteContact }) =>
+  contactsItem.map(({ id, name, number }) => (
+    <ContactItem key={id}>
+      <Text>{name}: {number}</Text>
+      <Button onClick={() => onDeleteContact(id)}>
+        Delete
+      </Button>
+    </ContactItem>
+  ));
+
+ContactListItem.propTypes = {
+    contacts: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired,
+        })
+    ),
+    onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactListItem;
